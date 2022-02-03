fb12ec5e3420be41c715e25f46bafd64b3bd4586
Matt Powers <citizenpowers@hotmail.com>
2022-02-03 16:42:12 -0500

    Updated water depth data

--- Water_Depth.R
+++ Water_Depth.R
@@ -42,26 +42,24 @@ Outflow_Stage_BK <-get_hydro(dbkey = "T1049", date_min="2021-06-08",date_max=as.
 #Deployment 1
 Water_Depth_20210609_Data <- bind_rows(Bare_Depth_20210609_Data,Typha_Depth_20210609_Data,Mixed_Depth_20210609_Data ,Chara_Depth_20210609_Data,Naiad_Depth_20210609_Data) %>%
 mutate(`Date Time`=mdy_hms(paste(Date," ",Time," "),tz="America/New_York"))  %>%
-filter(`Date Time`>"2021-06-09 11:30:00")
+filter(`Date Time`>"2021-06-09 11:30:00") %>%
+filter(`Date Time`<"2021-11-09 10:00:00")  
 
 #Deployment 2
 Water_Depth_20211123_Data <- bind_rows(Bare_Depth_20211123_Data,Typha_Depth_20211123_Data,Mixed_Depth_20211123_Data ,Chara_Depth_20211123_Data,Naiad_Depth_20211123_Data) %>%
 mutate(`Date Time`=mdy_hms(paste(Date," ",Time," "),tz="America/New_York"))  %>%
 filter(`Date Time`>"2021-11-23 12:00:00") %>%
-filter(`Date Time`<"2022-01-11 10:00:00")  
+filter(`Date Time`<"2022-01-11 10:30:00")  
 
 Inflow_outflow_data <-  setNames(as.data.frame(seq(from=ISOdate(2021,6,01,0,0,0,tz = "US/Eastern"), to=ISOdate(year(today()),month(today()),day(today()),0,0,0,tz = "US/Eastern"),by = "min")),"date") %>%
 left_join(Inflow_Stage_BK ,by="date") %>%  
 left_join(Outflow_Stage_BK,by="date") %>%
 fill(`G378C_T_STG_ft NGVD29`,`G379B_H_STG_ft NGVD29`) %>%
-mutate(`Inflow Est. Water Depth`=`G378C_T_STG_ft NGVD29`/3.28084-3,`Outflow Est. Water Depth`=`G379B_H_STG_ft NGVD29`/3.28084-3)  %>%
+mutate(`Inflow Est. Water Depth`=(`G378C_T_STG_ft NGVD29`-9.4)/3.28084,`Outflow Est. Water Depth`=(`G379B_H_STG_ft NGVD29`-9.4)/3.28084)  %>%
 select(date,`Inflow Est. Water Depth`,`Outflow Est. Water Depth`)  %>%
 pivot_longer(names_to = "Site",values_to="level",2:3)  %>%
 rename(`Date Time`="date") %>%
 filter(if_else(minute(`Date Time`) %in% c(0,30),TRUE,FALSE)) 
-
-  
-
 
 #join Data
 Water_Depth_Data <- bind_rows(Water_Depth_20210609_Data,Water_Depth_20211123_Data) %>%
@@ -72,12 +70,21 @@ pivot_longer(names_to = "Site",values_to="level",2:8)
 
 # Save data ---------------------------------------------------------------
 
-write.csv(Water_Depth_Data,"./Data/Levelogger/Water_Depth_Data.csv",row.names = FALSE)
+write.csv(Water_Depth_Data,"./Data/Levelogger/Water_Depth_Data.csv",row.names = FALSE)  
 
 # Figures -----------------------------------------------------------------
-ggplot(Water_Depth_Data,aes(`Date Time`,level,color=Site,fill=Site))+geom_line()+#geom_point(shape=21)+
-scale_y_continuous(breaks = pretty_breaks(n=10))  +
-scale_fill_brewer(palette = "Set2",direction = -1)+scale_color_brewer(palette = "Set2",direction = -1)+theme_bw()
+#Water Depth over Time
+ggplot(Water_Depth_Data,aes(`Date Time`,level*100,color=Site,fill=Site))+geom_line(size=1)+
+scale_y_continuous(breaks = pretty_breaks(n=10))+scale_x_datetime(date_breaks="1 month",labels = date_format("%b"))+
+scale_fill_brewer(palette = "Set2",direction = -1)+scale_color_brewer(palette = "Set2",direction = -1)+theme_bw()+
+labs(title="Water Depth at Sites",y="Water Depth (cm)",x="Date")
+
+#Water Depth 1 week
+ggplot(Water_Depth_Data,aes(`Date Time`,level*100,color=Site,fill=Site))+geom_line(size=1)+
+scale_y_continuous(breaks = pretty_breaks(n=10))+scale_x_datetime(date_breaks="1 day",labels = date_format("%b %d"),limits = as.POSIXct(c("2021-09-15 00:00:00","2021-09-22 00:00:00")))+
+scale_fill_brewer(palette = "Set2",direction = -1)+scale_color_brewer(palette = "Set2",direction = -1)+theme_bw()+
+labs(title="Water Depth at Sites",y="Water Depth (cm)",x="Date")
+
 
 
 
