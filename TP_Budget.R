6f2b08195a8f656891ac5bd3e6c135dff1742243
citizenpowers <citizenpowers@hotmail.com>
2022-06-21 15:55:18 -0400

    Finished TP budget

--- TP_Budget.R
+++ TP_Budget.R
@@ -39,19 +39,44 @@ TP_Budget <-Water_budget %>%
 select(`Date Time`,`Outflow (cfs)`) %>%  
 left_join(TP_Tidy,by="Date Time") %>%
 arrange(`Date Time`) %>%
-mutate(`Outflow (cfs)`=`Outflow (cfs)`*-1) %>% #discharged values are negative to reflect water leaving the cell. Converted to positive for TP budget  
+filter(`Date Time`>"2021-06-01 00:00:00") %>%
+filter(`Date Time`<"2022-05-24 24:00:00") %>%  
+mutate(`Outflow (cfs)`=`Outflow (cfs)`*-1) %>% #discharged values are negative to reflect water leaving the cell. Converted to positive for TP budget 
+mutate(`Cumulative Outflow`=cumsum(`Outflow (cfs)`))  %>%
 mutate(`Chara Int`=na.approx(Chara,na.rm = TRUE,rule=2)) %>%
 mutate(`Bare Int`=na.approx(Bare,na.rm = TRUE,rule=2)) %>%
 mutate(`Typha Int`=na.approx(Typha,na.rm = TRUE,rule=2)) %>%
-mutate(`Mixed Int`=na.approx(Mixed,na.rm = TRUE,rule=2)) %>%
 mutate(`Naiad Int`=na.approx(Naiad,na.rm = TRUE,rule=2)) %>%
-filter(`Date Time`>"2021-06-01 00:00:00") %>%
-filter(`Date Time`<"2022-05-24 24:00:00") %>%
-mutate(`Load Chara`=`Outflow (cfs)`*28.316847*60/1000000) %>% #convert to P load kg. 28.31 L/cf *60 sec * 1g/1000mg * 1kg/1000g
-mutate(`Cumulative TP Chara`=cumsum(`Load Chara`))
+mutate(`Mixed Int`=na.approx(Mixed,na.rm = TRUE,rule=2)) %>%  
+mutate(`Load Chara`=`Outflow (cfs)`*`Chara Int`*28.316847*60/1000000) %>% #convert to P load kg. 28.31 L/cf *60 sec * 1g/1000mg * 1kg/1000g
+mutate(`Load Bare`=`Outflow (cfs)`*`Bare Int`*28.316847*60/1000000) %>% #convert to P load kg. 28.31 L/cf *60 sec * 1g/1000mg * 1kg/1000g
+mutate(`Load Typha`=`Outflow (cfs)`*`Typha Int`*28.316847*60/1000000) %>% #convert to P load kg. 28.31 L/cf *60 sec * 1g/1000mg * 1kg/1000g
+mutate(`Load Naiad`=`Outflow (cfs)`*`Naiad Int`*28.316847*60/1000000) %>% #convert to P load kg. 28.31 L/cf *60 sec * 1g/1000mg * 1kg/1000g
+mutate(`Load Mixed`=`Outflow (cfs)`*`Mixed Int`*28.316847*60/1000000) %>% #convert to P load kg. 28.31 L/cf *60 sec * 1g/1000mg * 1kg/1000g
+mutate(`Cumulative TP Chara`=cumsum(`Load Chara`)) %>%
+mutate(`Cumulative TP Bare`=cumsum(`Load Bare`)) %>%  
+mutate(`Cumulative TP Typha`=cumsum(`Load Typha`)) %>%
+mutate(`Cumulative TP Naiad`=cumsum(`Load Naiad`)) %>% 
+mutate(`Cumulative TP Mixed`=cumsum(`Load Mixed`)) %>%
+mutate(`FWM Chara`=`Cumulative TP Chara`/(`Cumulative Outflow`*28.316847*60)*1000000)  %>%  #convert to FWM TP mg/L
+mutate(`FWM Bare`=`Cumulative TP Bare`/(`Cumulative Outflow`*28.316847*60)*1000000)  %>%
+mutate(`FWM Typha`=`Cumulative TP Typha`/(`Cumulative Outflow`*28.316847*60)*1000000) %>%
+mutate(`FWM Naiad`=`Cumulative TP Naiad`/(`Cumulative Outflow`*28.316847*60)*1000000) %>%
+mutate(`FWM Mixed`=`Cumulative TP Mixed`/(`Cumulative Outflow`*28.316847*60)*1000000) 
+
+
+write.csv(TP_Budget,"./Data/P Budget/TP_Budget.csv",row.names = FALSE)
+
 
 ggplot(Water_budget,aes(`Date Time`,`Outflow (cfs)`))+geom_point()
 
 
+Cumulative_TP <- TP_Budget %>%
+pivot_longer(19:23,names_to="Ecotope",values_to="Value")
 
+#All Analyses Concentration over time points and smooth
+ggplot(Cumulative_TP,aes(`Date Time`,`Value`,color=Ecotope))+geom_point()+
+scale_x_datetime()  
+ 
+scale_x_date(date_breaks="3 month",labels = date_format("%b %y"))+ ylab(" ")+guides(x =  guide_axis(angle = 40))
 
