2893a18b5e11901de4c8a5e1afafcd7bae26f3ed
citizenpowers <citizenpowers@hotmail.com>
2022-07-01 08:52:01 -0400

    Created correlation figs for WQ Analytes 1 WQ Analytes 2 Continuous Parameters

--- Correlation Analysis.R
+++ Correlation Analysis.R
@@ -15,6 +15,7 @@ library(zoo)
 library(readxl)
 library(corrplot)
 library(GGally)
+library(ggthemr)
 
 # Import Data -------------------------------------------------------------
 
@@ -22,11 +23,15 @@ library(GGally)
 
 WQ_Field_with_continuous_same_rows <- read.csv("./Data/Joined Data/WQ_Field_with_continuous_same_rows.csv",check.names=FALSE)
 
+# Theme -------------------------------------------------------------------
+
+ggthemr("flat dark",type="outer", layout="scientific")
+
 # Tidy Data ---------------------------------------------------------------
 
-Correlation_Data_Tidy <- WQ_Field_with_continuous_same_rows #%>% 
+Correlation_Data_Tidy <- WQ_Field_with_continuous_same_rows %>% 
+filter(Position=="Downstream")  
 #filter(TPO4<30) #remove right handed outliers
-
 
 
 # Correlation ---------------------------------------------
@@ -44,6 +49,18 @@ select(-`Date`,Ecotope,-Position,-Hour,-Minute) %>%
 select(TPO4,Ecotope,58:68)  %>%  #Select Physical parameters
 select(sort(current_vars())) 
 
+#Calculate DF of TPO4 and analytes
+TP_Analyte_Correlation_1 <- Correlation_Data_Tidy %>%
+select(-`Date`,Ecotope,-Position,-Hour,-Minute) %>%  
+select(TPO4,Ecotope,2:12)  %>%  #Select Analyte 
+select(sort(current_vars())) 
+
+TP_Analyte_Correlation_2 <- Correlation_Data_Tidy %>%
+select(-`Date`,Ecotope,-Position,-Hour,-Minute) %>%  
+select(TPO4,Ecotope,13:22)  %>%  #Select Analyte 
+select(sort(current_vars()))
+
+
 #Calculate correlation of all ecotopes grouped together
 TP_Correlation <- Correlation_Data_Tidy %>%
 select(-`Date`,-Ecotope,-Position,-Hour,-Minute) %>%  
@@ -51,6 +68,8 @@ select(1:28,57:76)  %>%  #remove upstream downstream differences column
 select(-OPO4,-`TSS mg/L`,-`BGA-PC RFU`,-`BGA-PC ug/L`,-`Chl ug/L`) %>%  #remove parameters that are missing data or has unchanging data
 select(sort(current_vars())) %>% #sorts column alphabetically
 cor(method="spearman",use = "pairwise.complete.obs")
+
+
 
 
 All_Correlation <- WQ_Field_with_continuous_same_rows %>%
@@ -80,11 +99,19 @@ ggpairs(TP_Physical_Correlation,ggplot2::aes(colour=Ecotope),method = "spearman"
 ggsave(plot = last_plot(),filename="./Figures/Correlogram- Physical Parameters.jpeg",width =13.333, height =7.5, units = "in")
 
 #Correlation Plot with GGALLY of sonde parameters
-ggpairs(TP_Continuous_Correlation,ggplot2::aes(colour=Ecotope),method = "spearman", title = "Correlation: Physical Parameters and TP") #TP04 Correlation with Physico-chemical parameters
+ggpairs(TP_Continuous_Correlation,ggplot2::aes(colour=Ecotope),method = "spearman", title = "Correlation: Continuous Parameters and TP") #TP04 Correlation with Physico-chemical parameters
 
-ggsave(plot = last_plot(),filename="./Figures/Correlogram- Physical Parameters.jpeg",width =13.333, height =7.5, units = "in")
+ggsave(plot = last_plot(),filename="./Figures/Correlogram- Continuous Parameters.jpeg",width =16, height =9, units = "in")
 
+#Correlation Plot with GGALLY of analyte 
+ggpairs(TP_Analyte_Correlation_1,ggplot2::aes(colour=Ecotope),method = "spearman", title = "Correlation: Water Quality Analytes") #TP04 Correlation with Physico-chemical parameters
 
+ggsave(plot = last_plot(),filename="./Figures/Correlogram- WQ Analytes 1.jpeg",width =16, height =9, units = "in")
+
+#Correlation Plot with GGALLY of analyte 
+ggpairs(TP_Analyte_Correlation_2,ggplot2::aes(colour=Ecotope),method = "spearman", title = "Correlation: Water Quality Analytes") #TP04 Correlation with Physico-chemical parameters
+
+ggsave(plot = last_plot(),filename="./Figures/Correlogram- WQ Analytes 2.jpeg",width =16, height =9, units = "in")
 
 #Correlation Plot with GGALLY 
 ggpairs(select(TP_differences_vs_Analytes,-Date,-Ecotope,-PH,-DO,-COND,-TEMP), title="correlogram with ggpairs()") #removed physico-chemical parameters since that data hasn't been entered yet.
