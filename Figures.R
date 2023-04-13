1544e15e2eeca549e9c5438007b5c7adc37ad338
citizenpowers <citizenpowers@hotmail.com>
2023-04-13 14:10:47 -0400

    Updated figures for presentation GEER23

--- Figures.R
+++ Figures.R
@@ -21,6 +21,7 @@ library(ggthemr)
 library(interp)
 library(tidyverse)
 library(stringr)
+library(ggrepel)
 
 # Import Data -------------------------------------------------------------
 
@@ -37,8 +38,8 @@ TP_Budget <- read_csv("./Data/P Budget/TP_Budget.csv")
 # Theme -------------------------------------------------------------------
 
 ggthemr("flat dark",type="outer", layout="scientific")
-Presentation_theme <- theme(strip.text = element_text(size=20) ,legend.position="bottom",axis.text=element_text(size=16),axis.title = element_text(size = 20),legend.text = element_text(size = 20),legend.title = element_text(size = 20))
-Presentation_theme2 <- theme(strip.text = element_text(size=20) ,legend.position="bottom",axis.text=element_text(size=14),axis.title = element_text(size = 16),legend.text = element_text(size = 20),legend.title = element_text(size = 20))
+Presentation_theme <- theme(strip.text = element_text(size=20) ,legend.position="bottom",axis.text=element_text(size=16),axis.title = element_text(size = 20),legend.text = element_text(size = 24),legend.title = element_text(size = 20))
+Presentation_theme2 <- theme( strip.text = element_text(size=20) ,legend.position="bottom",axis.text=element_text(size=14),axis.title = element_text(size = 16),legend.text = element_text(size = 20),legend.title = element_text(size = 20))
 
 
 # QC Blank Evaluation -----------------------------------------------------
@@ -103,7 +104,7 @@ scale_x_date(date_breaks="3 month",labels = date_format("%b %y"))+ ylab(" ")+gui
 
 ggsave(plot = last_plot(),filename="./Figures/All WQ Analytes over time.jpeg",width =13.333, height =7.5, units = "in")
 
-#TPO4 Concentration over time points and smooth
+#TPO4 Concentration over time points and smooth (STA1W and STA34)
 ggplot(filter(WQ_Data_Tidy,Position=="Downstream",TEST_NAME=="TPO4",Ecotope!="Naiad"),aes(Date,VALUE*1000,color=Ecotope,fill=Ecotope,linetype=Ecotope))+
 geom_point(aes(Date,VALUE*1000,color=Ecotope,fill=Ecotope),size=3)+
 geom_smooth(se=FALSE)+facet_wrap(~fct_rev(STA),nrow=2)+geom_hline(aes(yintercept = 13),color="white",linetype="dashed")+
@@ -113,6 +114,18 @@ scale_x_date(date_breaks="1 month",labels = date_format("%b %y"))+guides(x =  gu
 ggsave(plot = last_plot(),filename="./Figures/TPO4 over time-flat dark.jpeg",width =13.333, height =7.5, units = "in")
 
 ggsave(plot = last_plot(),filename="./Figures/TPO4 over time-SFER.jpeg",width =8, height =5.5, units = "in")
+
+
+#TPO4 Concentration over time points and smooth ( STA34)
+ggplot(filter(WQ_Data_Tidy,Position=="Downstream",TEST_NAME=="TPO4",Ecotope!="Naiad",STA=="STA-3/4 Cell 2B",Date<="2022-06-01"),aes(Date,VALUE*1000,color=Ecotope,fill=Ecotope,linetype=Ecotope))+
+geom_point(aes(Date,VALUE*1000,color=Ecotope,fill=Ecotope),size=4)+
+geom_smooth(se=FALSE)+facet_wrap(~fct_rev(STA),nrow=2)+geom_hline(aes(yintercept = 13),color="white",linetype="dashed")+
+Presentation_theme+scale_shape_manual(values = c(21:24)) + #scale_y_continuous(breaks=seq(0,100,10),limits=c(0,100))+
+scale_x_date(date_breaks="1 month",labels = date_format("%b %y"))+guides(x =  guide_axis(angle = 40))+labs(y=expression(TP~(mu~g~L^-1)))
+
+ggsave(plot = last_plot(),filename="./Figures/TPO4 over time-flat dark- STA34 only WY22.jpeg",width =13.333, height =7.5, units = "in")
+
+
 
 #TPO4 boxplots
 TPO4_boxplot <-ggplot(filter(WQ_Field_Data,Position=="Downstream"),aes(Ecotope,TPO4*1000,fill=Ecotope))+geom_boxplot()+
@@ -157,13 +170,12 @@ scale_x_datetime(date_breaks="1 month",labels = date_format("%b %y"),limits = as
 scale_fill_brewer(palette = "Set2",direction = -1)+scale_color_brewer(palette = "Set2",direction = -1)+theme_bw()
 
 #DCS Depth vs TP 
-ggplot(filter(WQ_Field_with_continuous_same_rows,Position=="Downstream",Ecotope!="Naiad",STA=="STA-3/4 Cell 2B"),aes(`DCS (Field Data)`,`TPO4`*1000,fill=Ecotope,color=Ecotope))+
-facet_wrap(~Ecotope)+scale_y_continuous(breaks=seq(0,80,10))+Presentation_theme2+coord_cartesian(ylim=c(0,80))+
+ggplot(filter(WQ_Field_with_continuous_same_rows,Position=="Downstream",Ecotope!="Naiad",STA=="STA-3/4 Cell 2B",Date<"2022-06-02"),aes(`DCS (Field Data)`,`TPO4`*1000,fill=Ecotope,color=Ecotope))+
+facet_wrap(~Ecotope)+scale_y_continuous(breaks=seq(0,80,10))+Presentation_theme2+coord_cartesian(ylim=c(0,40))+
 geom_rect(aes(xmin = 45.72, ymin = -Inf, xmax = 76.2, ymax = 80),alpha=.5,fill="#787C99",color="#787C99")+geom_point(shape=21,size=2,color="grey70")+geom_smooth()+
 ylab(expression(TP~(mu~g~L^-1)))+xlab("Depth to Consolidated Substrate (cm)")+guides(fill="none",color="none")
 
 ggsave(plot = last_plot(),filename="./Figures/TPO4 vs Water Depth.jpeg",width =5, height =6, units = "in")
-
 
 #DCS Depth vs Temp
 ggplot(WQ_Field_Data_Continuous_data,aes(`Average DCS (Field Data)`,`Temp`))+geom_point(shape=21,size=2)+geom_smooth()+theme_bw()+
@@ -244,8 +256,8 @@ facet_wrap(~Ecotope)+scale_y_continuous(breaks=seq(0,.05,0.005))+
 scale_fill_brewer(palette = "Set2",direction = -1)+scale_color_brewer(palette = "Set2",direction = -1)+theme_bw()
 
 #outflow vs TPO4
-ggplot(filter(WQ_Field_with_continuous_same_rows,Position=="Downstream",Ecotope!="Naiad",STA=="STA-3/4 Cell 2B"),aes(`Mean outflow (cfs)`,`TPO4`*1000,fill=Ecotope,color=Ecotope))+geom_point(shape=21,size=2,color="grey70")+
-facet_wrap(~Ecotope)+scale_y_continuous(breaks=seq(0,100,10))+Presentation_theme2+ coord_cartesian(ylim=c(0,80))+
+ggplot(filter(WQ_Field_with_continuous_same_rows,Position=="Downstream",Ecotope!="Naiad",STA=="STA-3/4 Cell 2B",Date<"2022-06-02"),aes(`Mean outflow (cfs)`,`TPO4`*1000,fill=Ecotope,color=Ecotope))+geom_point(shape=21,size=2,color="grey70")+
+facet_wrap(~Ecotope)+scale_y_continuous(breaks=seq(0,100,10))+Presentation_theme2+ coord_cartesian(ylim=c(0,40))+
 geom_ribbon(stat='smooth', method = "loess", se=TRUE, alpha=0.3) +
 geom_line(stat='smooth', method = "loess")  + 
 ylab(expression(TP~(mu~g~L^-1)))+xlab("Outflow (cfs)")+guides(fill="none",color="none")
@@ -276,8 +288,8 @@ ggsave(plot = last_plot(),filename="./Figures/TPO4 vs Flow vs Depth-SFER.jpeg",w
 ggsave(plot = last_plot(),filename="./Figures/TPO4 vs Flow vs Depth.jpeg",width =13.333, height =7.5, units = "in")
 
 # TP vs physico-chemical parameters ----------------------------------------
-ggplot(filter(WQ_Field_with_continuous_same_rows,Position=="Downstream",Ecotope!="Naiad",STA=="STA-3/4 Cell 2B"),aes(`Temp`,`TPO4`*1000,color=Ecotope,fill=Ecotope))+geom_smooth()+geom_point(shape=21,size=2,color="grey70")+
-facet_wrap(~Ecotope)+scale_y_continuous(breaks=seq(0,80,10))+Presentation_theme2+coord_cartesian(ylim=c(0,80))+
+ggplot(filter(WQ_Field_with_continuous_same_rows,Position=="Downstream",Ecotope!="Naiad",STA=="STA-3/4 Cell 2B",Date<"2022-06-02"),aes(`Temp`,`TPO4`*1000,color=Ecotope,fill=Ecotope))+geom_smooth()+geom_point(shape=21,size=2,color="grey70")+
+facet_wrap(~Ecotope)+scale_y_continuous(breaks=seq(0,80,10))+Presentation_theme2+coord_cartesian(ylim=c(0,40))+
 ylab(expression(TP~(mu~g~L^-1)))+xlab("Temp (C)")+guides(fill="none",color="none")
 
 ggsave(plot = last_plot(),filename="./Figures/TPO4 vs Temp.jpeg",width =5, height =6, units = "in")
@@ -331,11 +343,34 @@ facet_wrap(~TEST_NAME,scales = "free_y")+scale_fill_brewer(palette = "Set2",dire
 
 Cumulative_TP <- TP_Budget %>%
 pivot_longer(25:31,names_to="Ecotope",values_to="Value") %>%
-filter(row_number() %% 4==1) #Select every 4th row 
+filter(row_number() %% 4==1) %>% #Select every 4th row 
+slice_sample(n=1000) %>%
+mutate(`y axis`=case_when(Ecotope=="Cumulative TP Chara"~1595,
+                          Ecotope=="Cumulative TP Bare"~1935,
+                          Ecotope=="Cumulative TP Typha"~1860,
+                          Ecotope=="Cumulative TP Naiad"~1790,
+                          Ecotope=="Cumulative TP Mixed"~1740,
+                          Ecotope=="Cumulative TP G379D"~1790,
+                          Ecotope=="Cumulative TP G379B"~2350)) %>%
+mutate(`x axis`=case_when(Ecotope=="Cumulative TP Chara"~"2022-04-01 00:00:00",
+                          Ecotope=="Cumulative TP Bare"~"2022-04-01 00:00:00",
+                          Ecotope=="Cumulative TP Typha"~"2022-04-01 00:00:00",
+                          Ecotope=="Cumulative TP Naiad"~"2022-06-01 00:00:00",
+                          Ecotope=="Cumulative TP Mixed"~"2022-04-01 00:00:00",
+                          Ecotope=="Cumulative TP G379D"~"2022-04-01 00:00:00",
+                          Ecotope=="Cumulative TP G379B"~"2022-04-01 00:00:00")) %>%
+mutate(Ecotope=case_when(Ecotope=="Cumulative TP Chara"~"Chara",
+                            Ecotope=="Cumulative TP Bare"~"Bare",
+                            Ecotope=="Cumulative TP Typha"~"Typha",
+                            Ecotope=="Cumulative TP Naiad"~"Southern Naiad",
+                            Ecotope=="Cumulative TP Mixed"~"Naiad/Chara Mix",
+                            Ecotope=="Cumulative TP G379D"~"G379D",
+                            Ecotope=="Cumulative TP G379B"~"G379B")) %>%  
+mutate(`x axis`=ymd_hms(`x axis`))  
 
 #All Analyses Concentration over time points and smooth
-FWM_TP_PLot <- ggplot(Cumulative_TP,aes(`Date Time`,`Value`,color=Ecotope))+
-geom_line(aes(`Date Time`,`Outflow (cfs)`),color="white",linetype="dashed")+scale_y_continuous(sec.axis = sec_axis(~., name = "Outflow (cfs)"))+geom_point()+
+ggplot(filter(Cumulative_TP,Ecotope %in% c("Chara","Bare","Typha","Naiad/Chara Mix")),aes(`Date Time`,`Value`,color=Ecotope))+geom_text(aes(`x axis`,`y axis`,color=Ecotope,label=Ecotope)) +
+scale_y_continuous(sec.axis = sec_axis(~., name = "Outflow (cfs)"))+geom_line()+geom_line(aes(`Date Time`,`Outflow (cfs)`),color="white",linetype="dashed")+
 theme(legend.position="bottom",axis.text=element_text(size=14),axis.title = element_text(size = 16),legend.text = element_text(size = 14),legend.title = element_text(size = 16))+  
 scale_x_datetime(date_breaks="3 month",labels = date_format("%b %y"))+ ylab("P (kg)")+guides(x =  guide_axis(angle = 40),position="bottom")
 
