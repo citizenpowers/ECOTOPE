cb098d20e509f786855ddc9689bb12de9b5d5af0
citizenpowers <citizenpowers@hotmail.com>
2024-05-23 14:14:31 -0400

    Added bulk density to soils data

--- Soil_Tidy.R
+++ Soil_Tidy.R
@@ -18,29 +18,26 @@ library(zoo)
 
 # Import Data -------------------------------------------------------------
 
-
-WQ_Data <- read_excel("Data/WQ Data/WQ Data.xlsx",sheet = "Sheet1")
+Physical_Parameters <- read_excel("Data/Soils Data/Physical Parameters.xlsx")
 
 
 # Tidy Data ---------------------------------------------------------------
 
 
-Soils_data <- WQ_Data %>%
-filter(MATRIX %in% c("FLOC","SO")) %>%
-mutate(Date=as.Date(COLLECT_DATE)) %>%
-mutate(Ecotope=case_when(str_detect(STATION,"STA34C2B_C")~"Chara",
-                         str_detect(STATION,"STA34C2B_T")~"Typha",
-                         str_detect(STATION,"STA34C2B_N")~"Naiad",
-                         str_detect(STATION,"STA34C2B_M")~"Mixed",
-                         str_detect(STATION,"STA34C2B_B")~"Bare")) 
+#Find the moisture content of each sample. Needed to calculate the bulk density of sample.
+Moisture_content <- Physical_Parameters %>%  
+filter(TEST_NAME=="MOISTURE") %>%
+select(SAMPLE_ID,STATION,MATRIX,VALUE) %>%
+rename(`Moisture Content`="VALUE")
+
+Physical_Parameters_tidy <- Physical_Parameters %>%
+left_join(Moisture_content,by=c("SAMPLE_ID","STATION","MATRIX")) %>%  #join moisture content
+mutate(`Bulk Density`=ifelse(!is.na(`Bulk Density`),`Bulk Density`,`Wet Weight`*(1-`Moisture Content`/100)/(78.5*as.numeric(CORE_LENGTH)))) %>%
+select(SAMPLE_ID,STATION,MATRIX,VALUE,`Bulk Density`)
+
+
 # Save Data ---------------------------------------------------------------
 
-write.csv(Soils_data , "./Data/Soils Data/Soils_Data_Tidy.csv",row.names = FALSE)
+write.csv(Physical_Parameters_tidy , "./Data/Soils Data/Soils_Data_Tidy.csv",row.names = FALSE)
 
 
-
-Soils_boxplot <-ggplot(Soils_data,aes(y=VALUE,color=Ecotope,fill=Ecotope))+
-geom_boxplot(alpha=.5)+facet_wrap(~TEST_NAME,scale="free")
-
-ggthemr("flat dark",type="outer", layout="scientific")
-TPO4_boxplot
