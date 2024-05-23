e34ffdabb1eca00f64b12d3d4abf1a6ad83f8d53
citizenpowers <citizenpowers@hotmail.com>
2024-05-23 14:59:45 -0400

    fixed unit conversion error in storage calculation

--- Soil_Tidy.R
+++ Soil_Tidy.R
@@ -30,7 +30,7 @@ filter(TEST_NAME=="MOISTURE") %>%
 select(SAMPLE_ID,STATION,MATRIX,VALUE) %>%
 rename(`Moisture Content`="VALUE")
 
-Physical_Parameters_tidy <- Physical_Parameters %>%
+Soils_data <- Physical_Parameters %>%
 left_join(Moisture_content,by=c("SAMPLE_ID","STATION","MATRIX")) %>%  #join moisture content
 mutate(`Bulk Density`=ifelse(!is.na(`Bulk Density`),`Bulk Density`,`Wet Weight`*(1-`Moisture Content`/100)/(78.5*as.numeric(CORE_LENGTH)))) %>%
   mutate(Ecotope=case_when(str_detect(STATION,"STA34C2B_C")~"Chara",
@@ -42,12 +42,12 @@ mutate(`Bulk Density`=ifelse(!is.na(`Bulk Density`),`Bulk Density`,`Wet Weight`*
                            str_detect(STATION,"STA1WC5B_T")~"Typha",
                            str_detect(STATION,"STA1WC5B_M")~"Mixed",
                            str_detect(STATION,"STA1WC5B_B")~"Bare"))  %>%
-mutate(`Storage (g/m^2)`=`Bulk Density`*as.numeric(CORE_LENGTH))
+mutate(`Storage (g/m^2)`=ifelse(UNITS=="mg/Kg",`Bulk Density`*as.numeric(CORE_LENGTH)*VALUE/100,`Bulk Density`*as.numeric(CORE_LENGTH)*VALUE*10000))
 
 
 
 # Save Data ---------------------------------------------------------------
 
-write.csv(Physical_Parameters_tidy , "./Data/Soils Data/Soils_Data_Tidy.csv",row.names = FALSE)
+write.csv(Soils_data , "./Data/Soils Data/Soils_Data_Tidy.csv",row.names = FALSE)
 
 
