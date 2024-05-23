3d3d71a5f8702d826b89c325d0eb721fb73e794c
citizenpowers <citizenpowers@hotmail.com>
2024-05-23 14:34:06 -0400

    Calculalted storage data

--- Soil_Tidy.R
+++ Soil_Tidy.R
@@ -33,7 +33,17 @@ rename(`Moisture Content`="VALUE")
 Physical_Parameters_tidy <- Physical_Parameters %>%
 left_join(Moisture_content,by=c("SAMPLE_ID","STATION","MATRIX")) %>%  #join moisture content
 mutate(`Bulk Density`=ifelse(!is.na(`Bulk Density`),`Bulk Density`,`Wet Weight`*(1-`Moisture Content`/100)/(78.5*as.numeric(CORE_LENGTH)))) %>%
-select(SAMPLE_ID,STATION,MATRIX,VALUE,`Bulk Density`)
+  mutate(Ecotope=case_when(str_detect(STATION,"STA34C2B_C")~"Chara",
+                           str_detect(STATION,"STA34C2B_T")~"Typha",
+                           str_detect(STATION,"STA34C2B_N")~"Naiad",
+                           str_detect(STATION,"STA34C2B_M")~"Mixed",
+                           str_detect(STATION,"STA34C2B_B")~"Bare",
+                           str_detect(STATION,"STA1WC5B_C")~"Chara",
+                           str_detect(STATION,"STA1WC5B_T")~"Typha",
+                           str_detect(STATION,"STA1WC5B_M")~"Mixed",
+                           str_detect(STATION,"STA1WC5B_B")~"Bare"))  %>%
+mutate(`Storage (g/m^2)`=`Bulk Density`*as.numeric(CORE_LENGTH))
+
 
 
 # Save Data ---------------------------------------------------------------
