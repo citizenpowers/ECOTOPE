c4f7751918db128e9089971c62e00c4430385739
citizenpowers <citizenpowers@hotmail.com>
2024-08-21 15:38:26 -0400

    updated target stage in water dpeth figure

--- Figures.R
+++ Figures.R
@@ -333,7 +333,7 @@ mutate(Ecotope=factor(Ecotope,labels = c("Bare","italic(Chara)","Mixed","Naiad",
 #DCS Depth vs TP wet season vs dry season STA34
 ggplot(filter(Wet_vs_dry,Position=="Downstream",Ecotope!="Naiad",STA=="STA-3/4 Cell 2B"),aes(`DCS (Field Data)`,`TPO4`*1000,fill=Ecotope,color=Ecotope))+
 facet_grid(Season~Ecotope,labeller = labeller(.rows = label_value, .cols = label_parsed))+scale_y_continuous(breaks=seq(0,80,10))+Presentation_theme2+coord_cartesian(ylim=c(0,50))+
-geom_rect(aes(xmin = 45.72, ymin = -Inf, xmax = 76.2, ymax = 80),alpha=.5,fill="#e0ecf4",color="#e0ecf4")+
+geom_rect(aes(xmin = 38.1, ymin = -Inf, xmax = 45.7, ymax = 80),alpha=.5,fill="#e0ecf4",color="#e0ecf4")+
 geom_point(shape=21,size=2.5,color="grey70")+geom_smooth(span=10)+
 geom_hline(yintercept = 13,linetype="longdash",color="#785d37")+
 ylab(expression(TP~(mu~g~L^-1)))+xlab("Depth to Consolidated Substrate (cm)")+guides(fill="none",color="none")
